00e0ffa07dd182b3e3cfe2f2a7b4bd72ff5b0b8e
aaananas <anastasiia.len11@gmail.com>
2021-07-29 10:58:49 +0200

    added logger

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -1,6 +1,9 @@
 package taxi.service;
 
 import java.util.Optional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import taxi.exception.AuthenticationException;
 import taxi.lib.Inject;
 import taxi.lib.Service;
@@ -8,11 +11,13 @@ import taxi.model.Driver;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
     @Inject
     private DriverService driverService;
 
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
+        logger.debug("Method login was called. Params: login = {}", login);
         Optional<Driver> driver = driverService.getByLogin(login);
         if (driver.isPresent() && driver.get().getPassword().equals(password)) {
             return driver.get();
