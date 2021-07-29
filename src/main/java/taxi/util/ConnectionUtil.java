209ff67b3d71d39b8054e5e2801c51548e5002f4
aaananas <anastasiia.len11@gmail.com>
2021-07-29 10:43:54 +0200

    Added controllers, updated pom.xml

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -6,10 +6,11 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "DATABASE_URL";
-    private static final String USERNAME = "YOUR_USERNAME";
-    private static final String PASSWORD = "YOUR_PASSWORD";
-    private static final String JDBC_DRIVER = "JDBC_DRIVER";
+    private static final String URL = "jdbc:mysql://localhost:3306/taxi?"
+            + "useUnicode=true&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "7845";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
         try {
